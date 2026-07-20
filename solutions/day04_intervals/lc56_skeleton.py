06fec4c75e22f0fa264f744e64f1f06941501453
Ding Chen <dchen087@ucr.edu>
2026-07-19 22:58:33 -0700

    Day 4: intervals skeleton started

---
+++ solutions/day04_intervals/lc56_skeleton.py
@@ -0,0 +1,18 @@
+# LC 56 - Merge Intervals
+#
+# Given an array of intervals where intervals[i] = [start_i, end_i],
+# merge all overlapping intervals and return an array of the
+# non-overlapping intervals that cover all the intervals in the input.
+#
+# Example:
+# Input:  [[1,3],[2,6],[8,10],[15,18]]
+# Output: [[1,6],[8,10],[15,18]]
+
+def merge(intervals):
+    # step 1: sort by ___
+    # step 2: init result with ___
+    # step 3: for each interval:
+    #   if overlap: ___
+    #   else: ___
+    # return result
+    pass
